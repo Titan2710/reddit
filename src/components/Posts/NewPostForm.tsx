83be72cbd7dcff332f34b20459605f1b5dbb06bf
Wai Hlyan <waihlyanwin007@gmail.com>
2023-02-18 17:00:41 +0630

    hope

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Alert, AlertDescription, AlertIcon, AlertTitle, Flex, Icon, Text } from "@chakra-ui/react";
+import { Alert, AlertIcon, Flex, Icon, Text } from "@chakra-ui/react";
 import { IoDocumentText } from "react-icons/io5";
 import { IoImageOutline } from "react-icons/io5";
 import { BsLink45Deg, BsMic, BsPause } from "react-icons/bs";
@@ -9,6 +9,7 @@ import ImageUpload from "./PostForm/ImageUpload";
 import { Post } from "@/atoms/postsAtom";
 import { User } from "firebase/auth";
 import { useRouter } from "next/router";
+import { BiPoll } from "react-icons/bi";
 import {
   addDoc,
   collection,
@@ -18,7 +19,7 @@ import {
 } from "firebase/firestore";
 import { firestore, storage } from "@/firebase/clientApp";
 import { getDownloadURL, ref, uploadString } from "firebase/storage";
-import useSelectFile from './../../hooks/useSelectFile';
+import useSelectFile from "./../../hooks/useSelectFile";
 
 type NewPostFormProps = {
   user: User;
@@ -40,7 +41,7 @@ const formTabs = [
   },
   {
     title: "Poll",
-    icon: BsPause,
+    icon: BiPoll,
   },
   {
     title: "Talk",
@@ -50,7 +51,7 @@ const formTabs = [
 
 export type TabItem = {
   title: string;
-  icon: typeof Icon.argument;
+  icon: typeof Icon.arguments;
 };
 
 const NewPostForm: React.FC<NewPostFormProps> = ({
@@ -69,20 +70,22 @@ const NewPostForm: React.FC<NewPostFormProps> = ({
 
   const handleCreatePost = async () => {
     const { communityId } = router.query;
-    const newPost: Post = {
-      communityId: communityId as string,
-      communityImageURL: communityImageURL || "",
-      creatorId: user.uid,
-      creatorDisplayName: user.email!.split("@")[0],
-      title: textInputs.title,
-      body: textInputs.body,
-      numberOfComments: 0,
-      voteStatus: 0,
-      createdAt: serverTimestamp() as Timestamp,
-    };
+
     setLoading(true);
+    const { title, body } = textInputs;
     try {
-      const postDocRef = await addDoc(collection(firestore, "posts"), newPost);
+      const postDocRef = await addDoc(collection(firestore, "posts"), {
+        communityId,
+        communityImageURL: communityImageURL || "",
+        creatorId: user.uid,
+        userDisplayText: user.email!.split("@")[0],
+        numberOfComments: 0,
+        title,
+        body,
+        voteStatus: 0,
+        createdAt: serverTimestamp(),
+        editedAt: serverTimestamp(),
+      });
 
       if (selectedFile) {
         const imageRef = ref(storage, `posts/${postDocRef.id}/image`);
